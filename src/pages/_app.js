5902af70a08821c314ec90d338e1d2368e5024f4
Sh.Dilmaghani <shayan.dilmaghani@gmail.com>
2023-08-26 10:21:11 +0330

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import {AnimatePresence} from "framer-motion";
 import {useRouter} from "next/router";
+import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps }) {
     const router = useRouter()
     return (
         <>
             <AnimatePresence mode={'wait'}>
                 <Component key={router.asPath} {...pageProps} />
+                <Analytics />
             </AnimatePresence>
         </>
     )
