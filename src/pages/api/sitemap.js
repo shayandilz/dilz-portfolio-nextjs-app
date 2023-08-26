0611661f226450939540636eeda831b86ba5ff30
Sh.Dilmaghani <shayan.dilmaghani@gmail.com>
2023-08-26 11:42:54 +0330

    yoast-sitemap

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -1,10 +1,39 @@
-module.exports = async (req, res) => {
-    // Fetch the sitemap from the WordPress backend
-    const xmlRes = await fetch((process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL || "") + req.url);
-    let xml = await xmlRes.text();
+// pages/api/generate-sitemap.js
+import axios from 'axios';
+import { parseString } from 'xml2js';
+import xmlbuilder from 'xmlbuilder';
 
-    // Set the Content-Type to text/xml and send the XML content to the client
-    res.setHeader("Content-Type", "text/xml");
-    res.write(xml);
-    res.end();
+export default async (req, res) => {
+    try {
+        const sitemapIndexResponse = await axios.get('https://shayan.website/sitemap_index.xml');
+        const sitemapIndexXml = sitemapIndexResponse.data;
+
+        // Parse sitemap index XML
+        parseString(sitemapIndexXml, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            const sitemapUrls = result.sitemapindex.sitemap.map((entry) => entry.loc[0]);
+
+            // Fetch data from each sitemap URL and generate new XML content
+            const xmlContent = generateNewXmlContent(sitemapUrls);
+
+            res.setHeader('Content-Type', 'text/xml');
+            res.send(xmlContent);
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error generating new sitemap XML.');
+    }
 };
+
+function generateNewXmlContent(urls) {
+    const root = xmlbuilder.create('urlset', { version: '1.0', encoding: 'UTF-8' });
+    root.att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+    // Fetch data from URLs and generate XML entries
+    // Here, you would fetch each URL, extract necessary data, and add to the XML
+
+    return root.end({ pretty: true });
+}
