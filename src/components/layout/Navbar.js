5184fdf62e343bd6b76d1be1c4f933e91982b20a
Sh.Dilmaghani <shayan.dilmaghani@gmail.com>
2023-08-28 17:54:40 +0330

    framer motion

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -110,11 +110,11 @@ const Navbar = ({header, social}) => {
                         <CustomLinkMobile key={menuItems?.ID} href={menuItems.url} title={menuItems.title}
                                           icon={menuItems.icon} active={menuItems.icon_active} toggle={handleClick}/>
                     )) : null}
-                    <button className={`bg-white px-5 text-dark `} onClick={handleClick}>
+                    <button className={`bg-white flex justify-center items-center text-dark w-[4rem]`} onClick={handleClick}>
                         <svg xmlns="http://www.w3.org/2000/svg"
                              width="35"
                              height="35" fill="currentColor"
-                             className={`bi bi-arrow-up-short transition-all duration-500 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+                             className={`bi bi-arrow-up-short transition-all duration-500 ${isOpen ? 'rotate-180 scale-125' : 'rotate-0'}`}
                              viewBox="0 0 16 16">
                             <path fill-rule="evenodd"
                                   d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
