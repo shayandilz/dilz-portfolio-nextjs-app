0c4fb899faf874c98e7a8151267349dd5aaa3eee
Sh.Dilmaghani <shayan.dilmaghani@gmail.com>
2023-09-04 13:29:23 +0330

    hire us

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -8,7 +8,7 @@ const HireMe = ({email}) => {
             <div className={'flex items-center justify-center relative w-48 h-auto md:w-24'}>
                 <CircularText className={'fill-dark dark:fill-light animate-spin-slow'}/>
                 <Link href={'mailto:' + email} className={'absolute flex justify-center items-center left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border border-solid border-transparent bg-dark dark:bg-white text-light dark:text-dark dark:hover:bg-dark dark:hover:text-light h-20 w-20 rounded-full hover:bg-white hover:text-dark hover:border-dark transition md:w-12 md:h-12 md:text-[10px]'}>
-                    Hire me
+                    Hire us
                 </Link>
             </div>
         </div>
