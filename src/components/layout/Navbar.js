c5a990714613fb7691260188f6906dfa1742d6d2
Sh.Dilmaghani <shayan.dilmaghani@gmail.com>
2023-09-04 14:01:49 +0330

    jump scroll fixed

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -19,7 +19,7 @@ import useThemeSwitcher from "../hooks/useThemeSwitcher";
 const CustomLink = ({href, title, className = ""}) => {
     const router = useRouter()
     return (
-        <Link href={href} className={`${className} relative group`}>
+        <Link scroll={false} href={href} className={`${className} relative group`}>
             {title}
             <span
                 className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'} dark:bg-light`}
@@ -30,19 +30,15 @@ const CustomLink = ({href, title, className = ""}) => {
     )
 }
 
-const CustomLinkMobile = ({href, title, className = "", toggle, icon, active}) => {
+const CustomLinkMobile = ({ href, title, className = '', toggle, icon, active }) => {
     const router = useRouter();
-    const handleClick = () => {
-        router.push(href);
-    }
+
     return (
-        <button href={href} aria-label={title} className={`${className} relative group text-light dark:text-light my-4 text-2xl`}
-                onClick={handleClick}>
-            <div className={'transition duration-75 fill-light'}
-                 dangerouslySetInnerHTML={{__html: router.asPath === href ? active : icon}}/>
-        </button>
-    )
-}
+        <Link scroll={false} href={href} aria-label={title} className={`${className} relative group text-light dark:text-light my-4 text-2xl`}>
+            <div className={'transition duration-75 fill-light'} dangerouslySetInnerHTML={{ __html: router.asPath === href ? active : icon }} />
+        </Link>
+    );
+};
 const Navbar = ({header, social}) => {
     const [mode, setMode] = useThemeSwitcher();
     const [isOpen, setIsOpen] = useState(false)
@@ -134,7 +130,7 @@ const Navbar = ({header, social}) => {
                                         contact us :
                                     </span>
                                     {!isEmpty(social) && social.length ? social.map((socialItems, index) => (
-                                        <Link key={index} aria-label={socialItems.title} className={'w-9'}
+                                        <Link scroll={false} key={index} aria-label={socialItems.title} className={'w-9'}
                                               href={socialItems.url} target={'_blank'}>
                                             {renderSocialIcon(socialItems.title)}
                                         </Link>
