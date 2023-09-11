6f9ed2ffc6f402c389c72b87baf9932d19315bbb
Sh.Dilmaghani <shayan.dilmaghani@gmail.com>
2023-09-11 13:16:17 +0330

    google ana

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,21 +15,7 @@ export default function Document() {
                     }
           `}
             </Script>
-            <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-            />
 
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-            </Script>
             <Main/>
             <NextScript/>
             </body>
