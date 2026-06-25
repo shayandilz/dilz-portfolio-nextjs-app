1e5cb0d116d1f41cc9910fa98f73eafb4574c26a
shayandilmaghani <shayan.dilmaghani@gmail.com>
2026-06-25 12:43:53 +0330

    feat: configure output as standalone and format image remote patterns

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    output: 'standalone',
     reactStrictMode: true,
     images: {
-        remotePatterns:[
+        remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'shayan.website',
                 port: '',
                 pathname: '/**'
-            },{
+            }, {
                 protocol: 'https',
                 hostname: 'panel.shayan.website',
                 port: '',
